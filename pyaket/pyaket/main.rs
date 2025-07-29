7e32d8c0602b7e38f053be6ccafccde664c6e4bd
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-07-29 14:07:55 -0400

    Move the main.run function to the project itself

--- pyaket/pyaket/main.rs
+++ pyaket/pyaket/main.rs
@@ -1,134 +1,5 @@
 use pyaket::*;
 
-fn run(project: &PyaketProject) -> Result<()> {
-
-    // Send the executable path to Python, also flags a Pyaket app
-    let executable = std::env::current_exe()?.canonicalize()?;
-    envy::set("PYAKET", executable.display());
-
-    // Load environment variables where the shell (executable) is
-    for file in glob::glob("*.env").unwrap().map(|x| x.unwrap()) {
-        dotenvy::from_path(file)?;
-    }
-
-    envy::setdefault("UV_PYTHON_INSTALL_DIR", project.python_install_dir().display());
-    envy::setdefault("VIRTUAL_ENV",      project.installation_dir().display());
-    envy::setdefault("UV_CACHE_DIR",     project.uv_cache_dir().display());
-    envy::setdefault("UV_VENV_CLEAR",    1); // Skip destructive confirmation prompt
-    envy::setdefault("UV_SYSTEM_PYTHON", 0); // Always use a managed distribution
-    envy::setdefault("UV_NO_CONFIG",     1); // Do not look for a pyproject.toml
-
-    // Force disable the GIL on freethreaded python
-    if project.python.version.contains('t') {
-        envy::setdefault("UNSAFE_PYO3_BUILD_FREE_THREADED", 1);
-        envy::setdefault("PYTHON_GIL", 0);
-    }
-
-    if match read(project.uuid_tracker_file()) {
-        Ok(bytes) => {bytes != project.uuid.as_bytes()},
-        Err(_)    => true,
-    } || project.app.rolling {
-
-        /* Create the virtual environment */ {
-            let mut setup = project.uv()?;
-
-            setup.arg("venv")
-                .arg(project.installation_dir())
-                .arg("--python").arg(&project.python.version)
-                .arg("--seed").arg("--quiet");
-            if project.app.rolling {setup
-                .arg("--allow-existing");}
-            subprocess::run(&mut setup)?;
-        }
-
-        // Install PyTorch first, as other dependencies might
-        // install a platform's default backend
-        if !project.torch.version.is_empty() {
-            let mut torch = project.uv()?;
-
-            torch.arg("pip").arg("install")
-                .arg(format!("torch=={}", project.torch.version))
-                .arg(format!("--torch-backend={}", project.torch.backend))
-                .arg("--preview");
-
-            subprocess::run(&mut torch)?;
-        }
-
-        // Gets cleaned up when out of scope
-        let container = TempDir::with_prefix("pyaket-").unwrap();
-
-        let mut command = project.uv()?;
-        command.arg("pip").arg("install");
-        command.arg("--upgrade");
-        command.arg("pip");
-
-        // Write temp wheel/sdist packages and mark to install
-        for (name, bytes) in ["*.whl", "*.tar.gz"].into_iter()
-            .flat_map(|x| WheelAssets::glob(x).unwrap())
-        {
-            let file = container.child(name);
-            write(&file, bytes)?;
-            command.arg(&file);
-        }
-
-        // Add PyPI packages to be installed
-        if !project.app.pypi.is_empty() {
-            command.args(project.app.pypi.split(";"));
-        }
-
-        // Add the requirements.txt file to be installed
-        if !project.app.reqtxt.is_empty() {
-            let file = container.child("requirements.txt");
-            write(&file, &project.app.reqtxt)?;
-            command.arg("-r").arg(&file);
-        }
-
-        subprocess::run(&mut command)?;
-    }
-
-    // Flag this was a successful install
-    write(project.uuid_tracker_file(), &project.uuid)?;
-
-    /* ---------------------------------------- */
-    // Entry points
-
-    let mut main = project.uv()?;
-    main.arg("run");
-    main.arg("--no-project");
-    main.arg("--active");
-
-    if !project.entry.module.is_empty() {
-        main.arg("python")
-            .arg("-m").arg(&project.entry.module);
-
-    } else if !project.entry.script.is_empty() {
-        main.arg("run")
-            .arg(&project.entry.script);
-
-    } else if !project.entry.code.is_empty() {
-        main.arg("python")
-            .arg("-c").arg(&project.entry.code);
-
-    } else if !project.entry.command.is_empty() {
-        let args = shlex::split(&project.entry.command)
-            .expect("Failed to parse entry command");
-        main = Command::new(&args[0]);
-        main.args(&args[..]);
-
-    // Effectively a Python installer without entry points
-    } else {
-        main.arg("python");
-    }
-
-    // Passthrough incoming arguments
-    for arg in std::env::args().skip(1) {
-        main.arg(arg);
-    }
-
-    // Execute the main program
-    main.spawn()?.wait()?;
-    Ok(())
-}
 
 fn main() {
     LazyLock::force(&START_TIME);
@@ -136,8 +7,21 @@ fn main() {
 
     // Read the project configurion sent at the end of build.rs
     let project: PyaketProject = PyaketProject::from_json(env!("PYAKET_PROJECT"));
-    let runtime = run(&project);
 
+    // Self management command
+    #[cfg(feature="self")]
+    if let Some("self") = std::env::args().nth(1).as_deref() {
+        // Remove the first argument
+        let args: Vec<String> = std::env::args().skip(1).collect();
+        // Manager::try_parse_from(args).unwrap().run().unwrap();
+        match Manager::try_parse_from(args) {
+            Ok(manager) => manager.run().unwrap(),
+            Err(e) => eprintln!("Error: {}", e),
+        }
+        return;
+    }
+
+    let runtime = project.run();
     if runtime.is_err() {
         println!("\nError: {}", runtime.unwrap_err());
     }
--- pyaket/pyaket/project.rs
+++ pyaket/pyaket/project.rs
@@ -315,3 +315,137 @@ impl PyaketProject {
             .expect("uv executable not found")))
     }
 }
+
+/* -------------------------------------------------------------------------- */
+
+impl PyaketProject {
+    pub fn run(&self) -> Result<()> {
+
+        // Send the executable path to Python, also flags a Pyaket app
+        let executable = std::env::current_exe()?.canonicalize()?;
+        envy::set("PYAKET", executable.display());
+
+        // Load environment variables where the shell (executable) is
+        for file in glob::glob("*.env").unwrap().map(|x| x.unwrap()) {
+            dotenvy::from_path(file)?;
+        }
+
+        envy::setdefault("UV_PYTHON_INSTALL_DIR", self.python_install_dir().display());
+        envy::setdefault("VIRTUAL_ENV",      self.installation_dir().display());
+        envy::setdefault("UV_CACHE_DIR",     self.uv_cache_dir().display());
+        envy::setdefault("UV_VENV_CLEAR",    1); // Skip destructive confirmation prompt
+        envy::setdefault("UV_SYSTEM_PYTHON", 0); // Always use a managed distribution
+        envy::setdefault("UV_NO_CONFIG",     1); // Do not look for a pyproject.toml
+
+        // Force disable the GIL on freethreaded python
+        if self.python.version.contains('t') {
+            envy::setdefault("UNSAFE_PYO3_BUILD_FREE_THREADED", 1);
+            envy::setdefault("PYTHON_GIL", 0);
+        }
+
+        if match read(self.uuid_tracker_file()) {
+            Ok(bytes) => {bytes != self.uuid.as_bytes()},
+            Err(_)    => true,
+        } || self.app.rolling {
+
+            /* Create the virtual environment */ {
+                let mut setup = self.uv()?;
+
+                setup.arg("venv")
+                    .arg(self.installation_dir())
+                    .arg("--python").arg(&self.python.version)
+                    .arg("--seed").arg("--quiet");
+                if self.app.rolling {setup
+                    .arg("--allow-existing");}
+                subprocess::run(&mut setup)?;
+            }
+
+            // Install PyTorch first, as other dependencies might
+            // install a platform's default backend
+            if !self.torch.version.is_empty() {
+                let mut torch = self.uv()?;
+
+                torch.arg("pip").arg("install")
+                    .arg(format!("torch=={}", self.torch.version))
+                    .arg(format!("--torch-backend={}", self.torch.backend))
+                    .arg("--preview");
+
+                subprocess::run(&mut torch)?;
+            }
+
+            // Gets cleaned up when out of scope
+            let container = TempDir::with_prefix("pyaket-").unwrap();
+
+            let mut command = self.uv()?;
+            command.arg("pip").arg("install");
+            command.arg("--upgrade");
+            command.arg("pip");
+
+            // Write temp wheel/sdist packages and mark to install
+            for (name, bytes) in ["*.whl", "*.tar.gz"].into_iter()
+                .flat_map(|x| WheelAssets::glob(x).unwrap())
+            {
+                let file = container.child(name);
+                write(&file, bytes)?;
+                command.arg(&file);
+            }
+
+            // Add PyPI packages to be installed
+            if !self.app.pypi.is_empty() {
+                command.args(self.app.pypi.split(";"));
+            }
+
+            // Add the requirements.txt file to be installed
+            if !self.app.reqtxt.is_empty() {
+                let file = container.child("requirements.txt");
+                write(&file, &self.app.reqtxt)?;
+                command.arg("-r").arg(&file);
+            }
+
+            subprocess::run(&mut command)?;
+        }
+
+        // Flag this was a successful install
+        write(self.uuid_tracker_file(), &self.uuid)?;
+
+        /* ---------------------------------------- */
+        // Entry points
+
+        let mut main = self.uv()?;
+        main.arg("run");
+        main.arg("--no-project");
+        main.arg("--active");
+
+        if !self.entry.module.is_empty() {
+            main.arg("python")
+                .arg("-m").arg(&self.entry.module);
+
+        } else if !self.entry.script.is_empty() {
+            main.arg("run")
+                .arg(&self.entry.script);
+
+        } else if !self.entry.code.is_empty() {
+            main.arg("python")
+                .arg("-c").arg(&self.entry.code);
+
+        } else if !self.entry.command.is_empty() {
+            let args = shlex::split(&self.entry.command)
+                .expect("Failed to parse entry command");
+            main = Command::new(&args[0]);
+            main.args(&args[..]);
+
+        // Effectively pure python without entry points
+        } else {
+            main.arg("python");
+        }
+
+        // Passthrough incoming arguments
+        for arg in std::env::args().skip(1) {
+            main.arg(arg);
+        }
+
+        // Execute the main program
+        main.spawn()?.wait()?;
+        Ok(())
+    }
+}
